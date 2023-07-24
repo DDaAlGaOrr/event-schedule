4fc0ef79571fbd8cdf9ebe74973fbe3b0345d7e3
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2023-07-23 17:21:21 -0700

    rename interfaces

--- src/events/event.interface.ts
+++ src/events/event.interface.ts
@@ -1,17 +1,21 @@
 import { events } from './models/event.model'
 
-export interface ApiResponse {
+export interface ResponseInterface {
     statusCode: number
     message: string
     events?: events[]
     event?: events
 }
 
-export interface CreateEvent {
+export interface EventInterface {
     event_id?: number
     title: string
     description: string
     start_time: string
     end_time: string
     location: string
+}
+
+export interface IdParamInterface {
+    id: number
 }
