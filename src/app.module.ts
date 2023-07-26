629259e438bce5817f54418c6e32c35f79ca5b3b
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2023-07-26 16:52:30 -0700

    env variables implemented for db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { ConfigModule } from '@nestjs/config'
 
 import { EventModule } from './events/events.module'
-import sequelizeConfig from 'sequelize.config'
+import { events } from './events/models/event.model'
 
 @Module({
-    imports: [EventModule, SequelizeModule.forRoot(sequelizeConfig)],
+    imports: [
+        ConfigModule.forRoot(),
+        EventModule,
+        SequelizeModule.forRoot({
+            dialect: 'mysql',
+            host: process.env.DATABASE_HOST,
+            port: 3306,
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            models: [events],
+            autoLoadModels: true,
+        }),
+    ],
 })
 export class AppModule {}
