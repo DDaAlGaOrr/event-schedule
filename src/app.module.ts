c1dba9f0566159d1b05ec0d7bbbf105051befd05
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2023-07-26 17:45:55 -0700

    change port database

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { events } from './events/models/event.model'
         SequelizeModule.forRoot({
             dialect: 'mysql',
             host: process.env.DATABASE_HOST,
-            port: 3306,
+            port: 6101,
             username: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE_NAME,
